5521aa908923ced7c9106781ad44a319de9e7c2f
RAMI REDDY VATRAPU <vatrapuramireddy@gmail.com>
2023-10-23 14:22:54 +0530

    average rating fixed

--- frontend/src/components/common/StarRating.js
+++ frontend/src/components/common/StarRating.js
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const StarRating = ({ rating }) => {
+  console.log(rating)
   const fullStars = Math.floor(rating);
   const fractionalStar = rating - fullStars;
+  console.log(fractionalStar)
 
   // Define a CSS class for the star container to arrange stars horizontally
   const starContainerStyle = {
@@ -36,31 +38,32 @@ const StarRating = ({ rating }) => {
 
   // Create the fractional star with a mask and linear gradient
   if (fractionalStar > 0) {
-    const maskId = `mask-${fullStars}`;
+    const maskId = `mask-${fullStars}-${fractionalStar}`;
     const maskWidth = 24 * fractionalStar;
-    
+  
     stars.push(
       <svg
-        key="half"
+        key={`half-${fullStars}-${fractionalStar}`}
         className="star"
         style={{ ...starStyle }}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
       >
         <defs>
-          <linearGradient id="star-gradient" x1="0" x2="1" y1="0" y2="0">
+          <linearGradient id={`star-gradient-${fullStars}-${fractionalStar}`} x1="0" x2="1" y1="0" y2="0">
             <stop offset="0%" stopColor="gold" />
             <stop offset={`${fractionalStar * 100}%`} stopColor="gold" />
             <stop offset={`${fractionalStar * 100}%`} stopColor="transparent" />
           </linearGradient>
         </defs>
         <mask id={maskId}>
-          <path d="M12 2l2.4 7.2H21l-6.1 4.8 2.3 7.2-6-4.8-6 4.8 2.3-7.2L3 9.2H9.6z" fill="url(#star-gradient)" />
+          <path d="M12 2l2.4 7.2H21l-6.1 4.8 2.3 7.2-6-4.8-6 4.8 2.3-7.2L3 9.2H9.6z" fill={`url(#star-gradient-${fullStars}-${fractionalStar})`} />
         </mask>
         <path d="M12 2l2.4 7.2H21l-6.1 4.8 2.3 7.2-6-4.8-6 4.8 2.3-7.2L3 9.2H9.6z" fill="gold" mask={`url(#${maskId})`} />
       </svg>
     );
   }
+  
 
   return <div className="star-rating" style={starContainerStyle}>{stars}</div>;
 };
